fed606b2b70e309a53d24f99c1632ad4d63733ec
Andrew Miller <206alm@gmail.com>
2020-12-11 11:14:36 -0800

    bookshelves are built

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { List, ListItem } from '../components/List';
 import API from '../utils/API';
+import { Fade } from 'react-reveal';
 
 
 function Home() {
@@ -38,66 +39,66 @@ function Home() {
 
     return (
     <div>
-        <div className="flex-row">
-            <div className="flex-col">
-                <h2>C</h2>
-                <h2>R</h2>
-                <h2>I</h2>
-                <h2>M</h2>
-                <h2>E</h2>
-            </div>
-            <ul className="book-row">
-                {books.filter(book => book.subject.includes('Crime')).map((book, index) => {
-                    return (
-                        <div 
-                            key={book.cover_edition_key}
-                            className="book-cover"
-                        >
-                            <img 
-                                src={`http://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`} 
-                                alt={book.cover_edition_key}
-                                onClick={selectBook}
-                            />
-                        </div>
-                    )
-                })}
-                {console.log(books.filter(book => book.subject.includes('Crime')))}
-            </ul>
+        <div className="flex-col">
+            <Fade>
+                <div className="genre-titles">
+                    <h1>Crime</h1>    
+                </div>
+            </Fade>
+            
+            <Fade bottom cascade>
+                <ul className="book-row">
+                    {books.filter(book => book.subject.includes('Crime')).map((book, index) => {
+                        return (
+                            <div 
+                                key={book.cover_edition_key}
+                                className="book-cover"
+                            >
+                                <img 
+                                    src={`http://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`} 
+                                    alt={book.cover_edition_key}
+                                    onClick={selectBook}
+                                />
+                            </div>
+                        )
+                    })}
+                    {console.log(books.filter(book => book.subject.includes('Crime')))}
+                </ul>
+            </Fade>
         </div>
-        <div className="flex-row">
-            <div className="flex-col">
-                <h2>F</h2>
-                <h2>A</h2>
-                <h2>N</h2>
-                <h2>T</h2>
-                <h2>A</h2>
-            </div>
-            <ul className="book-row">
-                {books.filter(book => book.subject.includes('Biography')).map((book, index) => {
-                    return (
-                        <div 
-                            key={book.cover_edition_key}
-                            className="book-cover"
-                        >
-                            <img 
-                                src={`http://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`} 
-                                alt={book.cover_edition_key}
-                                onClick={selectBook}
-                            />
-                        </div>
-                    )
-                })}
-                {console.log(books.filter(book => book.subject.includes('Crime')))}
-            </ul>
+        <div className="flex-col">
+            <Fade>
+                <div className="genre-titles">
+                    <h1>Biography</h1>    
+                </div>
+            </Fade>
+            <Fade bottom cascade>
+                <ul className="book-row">
+                    {books.filter(book => book.subject.includes('Biography')).map((book, index) => {
+                        return (
+                            <div 
+                                key={book.cover_edition_key}
+                                className="book-cover"
+                            >
+                                <img 
+                                    src={`http://covers.openlibrary.org/b/olid/${book.cover_edition_key}.jpg`} 
+                                    alt={book.cover_edition_key}
+                                    onClick={selectBook}
+                                />
+                            </div>
+                        )
+                    })}
+                    {console.log(books.filter(book => book.subject.includes('Crime')))}
+                </ul>
+            </Fade>
         </div>
-        <div className="flex-row">
-            <div className="flex-col">
-                <h2>T</h2>
-                <h2>H</h2>
-                <h2>R</h2>
-                <h2>I</h2>
-                <h2>L</h2>
-            </div>
+        <div className="flex-col">
+            <Fade>
+                <div className="genre-titles">
+                    <h1>Thriller</h1>    
+                </div>
+            </Fade>
+            <Fade bottom cascade>
             <ul className="book-row">
                 {books.filter(book => book.subject.includes('Thriller')).map((book, index) => {
                     return (
@@ -114,15 +115,15 @@ function Home() {
                     )
                 })}
             </ul>
+            </Fade>
         </div>
-        <div className="flex-row">
-            <div className="flex-col">
-                <h2>C</h2>
-                <h2>H</h2>
-                <h2>I</h2>
-                <h2>L</h2>
-                <h2>D</h2>
-            </div>
+        <div className="flex-col">
+            <Fade>
+                <div className="genre-titles">
+                    <h1>Childrens</h1>    
+                </div>
+            </Fade>
+            <Fade bottom cascade>
             <ul className="book-row">
                 {books.filter(book => book.subject.includes('Children')).map((book, index) => {
                     return (
@@ -139,6 +140,7 @@ function Home() {
                     )
                 })}
             </ul>
+            </Fade>
         </div>
     </div>
     )
